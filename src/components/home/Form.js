f5be370361934df3889b3d6262982dbae3132ccb
Ziva <birulangit1996@gmail.com>
2021-06-07 07:46:43 +0700

    first commit

---
+++ src/components/home/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div className="section">
+      <form action="">
+        <h5 className="grey-text">New Note</h5>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
